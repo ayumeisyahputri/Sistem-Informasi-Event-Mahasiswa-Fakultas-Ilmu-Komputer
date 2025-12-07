4dbf43043a62019ea0870b617b04e4c83ee9b5c9
razkha <mohammadanandaadithyarazkha@razkha.local>
2025-12-07 18:00:33 +0700

    Resolve merge conflicts

--- app/Filters/AdminFilter.php
+++ app/Filters/AdminFilter.php
@@ -1,20 +1,23 @@
-<?php namespace App\Filters;
+<?php
+
+namespace App\Filters;
 
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
-class AuthGuard implements FilterInterface
+class AdminFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        if (! session()->get('user')) {
-            return redirect()->to('/login')->with('error', 'Silakan login terlebih dahulu.');
+        // Cek apakah user admin
+        if (session()->get('role') !== 'admin') {
+            return redirect()->to('/login')->with('error', 'Akses ditolak');
         }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        // nothing
+        // Optional
     }
 }
--- app/Views/Layout/footer.php
+++ app/Views/Layout/footer.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+</div> <!-- end container -->
+<footer style="text-align:center; padding:20px; color:#777;">
+    &copy; <?= date('Y') ?> Sistem Informasi Event Mahasiswa
+</footer>
+</body>
+</html>
+=======
 </div> <!-- TUTUP CONTAINER -->
 
 <!-- MODAL LOGOUT -->
